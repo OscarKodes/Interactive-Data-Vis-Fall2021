cd9f3a61e595fff42d74536509d845db71f10617
Oscar Ko <OscarKoEmail@gmail.com>
2021-09-04 16:11:57 -0400

    Add counter display and counter button.

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+
+// Declare Variables ================
+
+let count = 0;
+let countDisplay = document.querySelector(".counter-display");
+const addBtn = document.querySelector(".add-btn");
+
+
+// Add Event Listeners ==============
+
+addBtn.onclick = () => {
+    count++;
+    updateDisplay();
+}
+
+
+// Declare Functions ================
+
+const updateDisplay = () => countDisplay.innerText = count;
+
+
+// Call Function on Startup =========
+
+updateDisplay();
