50f7d1970d2ba7e06df2fac2cd0d012d371fb479
Oscar Ko <OscarKoEmail@gmail.com>
2021-09-04 17:41:29 -0400

    Add inputs, save as object, display object info

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,24 +1,47 @@
 
-// Declare Variables ================
+// DECLARE VARIABLES ==========================================
 
+// Variables for counter
 let count = 0;
 let countDisplay = document.querySelector(".counter-display");
 const addBtn = document.querySelector(".add-btn");
 
+// Variables for input form
+const nameInput = document.querySelector("#name-input");
+const emailInput = document.querySelector("#email-input");
+const driveInput = document.querySelector("#drive-input");
+const submitBtn = document.querySelector(".submit-btn");
 
-// Add Event Listeners ==============
+const nameDisplay = document.querySelector(".name-display");
+const emailDisplay = document.querySelector(".email-display");
+const driveDisplay = document.querySelector(".drive-display");
 
+let userObj;
+
+
+// ADD EVENT LISTENERS ====================================
+
+// click add button to increment by 1
 addBtn.onclick = () => {
     count++;
-    updateDisplay();
-}
+    countDisplay.innerText = count;
+};
+
+// click to display submitted info
+submitBtn.onclick = () => {
+
+    // Save submitted information into object
+    userObj = {
+        myName: nameInput.value,
+        email: emailInput.value,
+        license: driveInput.checked
+    };
+
+    // Display object's information as inner text
+    nameDisplay.innerText = `Name: ${userObj.myName}`;
+    emailDisplay.innerText = `Email: ${userObj.email}`;
+    driveDisplay.innerText = `License: ${userObj.license ? 'Yes' : 'No'}`;
+};
 
 
-// Declare Functions ================
 
-const updateDisplay = () => countDisplay.innerText = count;
-
-
-// Call Function on Startup =========
-
-updateDisplay();
