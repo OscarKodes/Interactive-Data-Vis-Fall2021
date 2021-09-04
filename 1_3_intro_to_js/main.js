32c06ea5107a0005205cdb5539c44928da4ca6fb
Oscar Ko <OscarKoEmail@gmail.com>
2021-09-04 19:12:58 -0400

    Add D3.js

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,31 +1,32 @@
 
-// DECLARE VARIABLES ==========================================
+// COUNTER ==========================================
 
-// Variables for counter
+// Variables 
 let count = 0;
 let countDisplay = document.querySelector(".counter-display");
 const addBtn = document.querySelector(".add-btn");
 
-// Variables for input form
-const nameInput = document.querySelector("#name-input");
-const emailInput = document.querySelector("#email-input");
-const driveInput = document.querySelector("#drive-input");
-const submitBtn = document.querySelector(".submit-btn");
-
-const nameDisplay = document.querySelector(".name-display");
-const emailDisplay = document.querySelector(".email-display");
-const driveDisplay = document.querySelector(".drive-display");
-
-let userObj;
-
-
-// ADD EVENT LISTENERS ====================================
 
 // click add button to increment by 1
 addBtn.onclick = () => {
     count++;
     countDisplay.innerText = count;
 };
+
+
+
+// INPUT FORMS INTO SAVED OBJECT =====================
+
+// Variables 
+const nameInput = document.querySelector("#name-input");
+const emailInput = document.querySelector("#email-input");
+const driveInput = document.querySelector("#drive-input");
+const submitBtn = document.querySelector(".submit-btn");
+const nameDisplay = document.querySelector(".name-display");
+const emailDisplay = document.querySelector(".email-display");
+const driveDisplay = document.querySelector(".drive-display");
+let userObj;
+
 
 // click to display submitted info
 submitBtn.onclick = () => {
@@ -42,6 +43,29 @@ submitBtn.onclick = () => {
     emailDisplay.innerText = `Email: ${userObj.email}`;
     driveDisplay.innerText = `License: ${userObj.license ? 'Yes' : 'No'}`;
 };
+
+
+// D3 EXPERIMENTATION ================================
+
+const colorBtn = document.querySelector(".color-btn");
+const bgColorBtn = document.querySelector(".bg-color-btn")
+
+const randomColor = (s, l) => `hsl( ${Math.random() * 360}, ${s}%, ${l}%`;
+
+colorBtn.onclick = () => {
+    d3.selectAll(".d3-play p")
+    .style("color", randomColor(100, 30));
+}
+
+bgColorBtn.onclick = () => {
+    d3.select("body")
+    .style("background-color", randomColor(25, 90));
+}
+
+
+
+
+
 
 
 
