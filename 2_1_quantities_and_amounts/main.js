b0f727b0833322da40d398848ac174f3b914879d
Oscar Ko <OscarKoEmail@gmail.com>
2021-09-26 16:55:23 -0400

    Create horizontal barchart with colors and axis labels

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,17 +1,60 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+const width = window.innerWidth *.8;
+const height = 300;
+const pastel1Colors = d3.scaleOrdinal(d3.schemePastel1);
+const pastel2Colors = d3.scaleOrdinal(d3.schemePastel2);
 
 /* LOAD DATA */
-d3.csv('./squirrelActivities.csv', d3.autoType)
+d3.csv('./swiftSales.csv', d3.autoType)
   .then(data => {
     console.log("data", data)
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
+    const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.usSales)])
+    .range([0, width]) // visual variable
+
+    const yScale = d3.scaleBand()
+    .domain(data.map(d => d.album))
+    .range([0, height])
+    .paddingInner(.2)
+
+    // AXIS
+    const xAxis = d3.axisBottom()
+    .scale(xScale);
+
+    const yAxis = d3.axisLeft()
+    .scale(yScale);
     
 
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
+    
+    // svg display area
+    const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width + 100)
+    .attr("height", height + 100)
 
-  })
+    // bars
+    svg.selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("width", d => xScale(d.usSales))
+    .attr("height", yScale.bandwidth())
+    .attr("x", 0)
+    .attr("y", d => yScale(d.album))
+    .attr("fill", pastel1Colors)
+    .attr("transform", "translate(100, 0)");
+
+    // xAxis labels
+    svg.append("g")
+    .attr("transform", "translate(100, 300)")
+    .call(xAxis);
+    
+    // yAxis labels
+    svg.append("g")
+    .attr("transform", "translate(100, 0)")
+    .call(yAxis);
+  });
