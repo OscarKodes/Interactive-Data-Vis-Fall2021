c1b6e5678d0893fc961c4007e5794cac69feec7d
Oscar Ko <OscarKoEmail@gmail.com>
2021-09-30 20:10:29 -0400

    Make width and height of SVG the same. Make margin only 50.

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 60, left: 60, right: 40 };
+const width = window.innerWidth * 0.8,
+  height = width,
+  margin = 50;
 
 /* LOAD DATA */
 d3.csv("Movie-Ratings.csv", d3.autoType).then(data => {
@@ -22,12 +22,12 @@ d3.csv("Movie-Ratings.csv", d3.autoType).then(data => {
   // xscale  - linear,count
   const xScale = d3.scaleLinear()
     .domain([0, d3.max(data.map(d => d["Rotten Tomatoes Ratings %"]))])
-    .range([margin.left, width - margin.right])
+    .range([margin, width - margin])
 
   // yscale - linear,count
   const yScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d["Audience Ratings %"])])
-    .range([height - margin.bottom, margin.top])
+    .range([height - margin, margin])
 
   // color scale
   const colorScale = d3.scaleOrdinal()
@@ -45,19 +45,20 @@ d3.csv("Movie-Ratings.csv", d3.autoType).then(data => {
     .append("svg")
     .attr("width", width)
     .attr("height", height)
+    .style("background-color", "lavender")
 
 
   // Axis Scales ---------------------
   const xAxis = d3.axisBottom(xScale)
 
   svg.append("g")
-    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .attr("transform", `translate(0,${height - margin})`)
     .call(xAxis);
   
   const yAxis = d3.axisLeft(yScale)
 
   svg.append("g")
-    .attr("transform", `translate(${margin.left},0)`)
+    .attr("transform", `translate(${margin},0)`)
     .call(yAxis);
 
   svg.append("text")
