f3242ecb5c8322507e90fb399bcc374b05d89bdc
Oscar Ko <OscarKoEmail@gmail.com>
2021-10-13 16:13:02 -0400

    Exploring demo code

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+const width = window.innerWidth * 0.8,
+  height = window.innerHeight * 0.8,
+  margin = { top: 25, bottom: 50, left: 50, right: 25 };
 
 /**
  * LOAD DATA
@@ -10,18 +10,61 @@
 Promise.all([
   d3.json("usState.json"),
   d3.csv("stateCapitals.csv", d3.autoType),
-]).then(([geojson, capitals]) => {
+]).then(([usMapData, capitalsData]) => {
   
-  console.log([geojson, capitals])
-  // SPECIFY PROJECTION
- 
+  console.log([usMapData, capitalsData])
 
-  // DEFINE PATH FUNCTION
+  // CREATE SCALES / PROJECTIONS
+  const projection = d3.geoAlbersUsa()
+    .fitSize([width - margin.left - margin.right,
+              height - margin.top - margin.bottom],
+              usMapData);
 
+  // CREATE SVG
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-  // APPEND GEOJSON PATH  
+  // PATH GENERATOR FOR US MAP
+  const pathGen = d3.geoPath(projection);
+
+  // DRAW EMPTY US MAP
+  const states = svg.selectAll(".state-path")
+      .data(usMapData.features)
+      .join("path")
+      .attr("class", "state-path")
+      .attr("stroke", "black")
+      .attr("fill", "transparent")
+      .attr("d", pathGen)
   
-  
-  // APPEND DATA AS SHAPE
+  // PLACE POINT FOR GRAD CENTER
+  const gradCenterCoordinates =  { latitude: 40.7423, longitude: -73.9833 };
+  svg.selectAll(".gradcenter-point")
+    .data([gradCenterCoordinates])
+    .join("circle")
+    .attr("class", "gradcenter-point")
+    .attr("r", 10)
+    .attr("fill", "gold")
+    .attr("transform", d => {
+
+      const [x, y] = projection([d.longitude, d.latitude])
+
+      return `translate(${x}, ${y})`
+    });
+
+  // DRAW STATE CAPITAL POINTS
+  svg.selectAll(".capital-points")
+    .data(capitalsData)
+    .join("circle")
+    .attr("class", "capital-points")
+    .attr("r", 5)
+    .attr("fill", "lightsalmon")
+    .attr("transform", d => {
+
+      const [x, y] = projection([d.longitude, d.latitude])
+
+      return `translate(${x}, ${y})`
+    });
 
 });
