1dfbe5201787ae149543e4c83a3ea526cb687587
Oscar Ko <OscarKoEmail@gmail.com>
2021-10-25 18:18:20 -0400

    Basic bar graph

--- 3_1_quantities_and_amounts/main.js
+++ 3_1_quantities_and_amounts/main.js
@@ -1,102 +1,65 @@
-// /* CONSTANTS AND GLOBALS */
-// // const width = ,
-// //   height = ,
-// //   margin = ,
-// //   radius = ;
-
-// // // since we use our scales in multiple functions, they need global scope
-// // let xScale, yScale;
-
-// /* APPLICATION STATE */
-// let state = {
-//   // data: [],
-// };
-
-// /* LOAD DATA */
-// d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
-//   console.log("data", raw_data);
-//   // save our data to application state
-//   state.data = raw_data;
-//   init();
-// });
-
-// /* INITIALIZING FUNCTION */
-// // this will be run *one time* when the data finishes loading in
-// function init() {
-//   /* SCALES */
-
-
-
-//   draw(); // calls the draw function
-// }
-
-// /* DRAW FUNCTION */
-// // we call this every time there is an update to the data/state
-// function draw() {
-//   /* HTML ELEMENTS */
- 
-
-
-// }
-
-
-// ====================================================
-
-/* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 60, left: 60, right: 40 },
+// CONSTANTS ###############################
+const width = window.innerWidth * 0.8,
+  height = window.innerWidth * 0.8,
+  margin = {
+    top: 30,
+    left: 50,
+    right: 50,
+    bottom: 60
+  },
   radius = 5;
-// since we use our scales in multiple functions, they need global scope
+
 let xScale, yScale;
 
-/* APPLICATION STATE */
+// APPLICATION STATE ########################
 let state = {
   data: [],
 };
 
-/* LOAD DATA */
+// LOAD DATA ###############################
 d3.csv('CLEANED_OscarRaces.csv', d3.autoType).then(raw_data => {
   console.log("data", raw_data);
-  // save our data to application state
   state.data = raw_data;
   init();
 });
 
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
+// INITIALIZING FUNCTION ###################
 function init() {
-  /* SCALES */
-  // xscale - categorical, activity
+  
+  // SCALES ----------------------------------
+  
   xScale = d3.scaleBand()
-    .domain(state.data.map(d=> d.races))
-    .range([0, width]) // visual variable
-    .paddingInner(.2)
+    .domain(state.data.map(d => d.races))
+    .range([margin.left, width - margin.right])
+    .paddingInner(.2);
 
-    // yscale - linear,count
   yScale = d3.scaleLinear()
-    .domain([0, d3.max(state.data, d=> d.count)])
-    .range([height, 0])
+    .domain(d3.extent(state.data, d => d.count))
+    .range([height - margin.top, margin.bottom])
+    .nice();
 
-  draw(); // calls the draw function
+  draw(); 
 }
 
-/* DRAW FUNCTION */
-// we call this every time there is an update to the data/state
+// DRAW FUNCTION ###############################
 function draw() {
-  /* HTML ELEMENTS */
-  // svg
+  
+  // HTML ELEMENTS ----------------------------
+
+  // SVG
   const svg = d3.select("#container")
     .append("svg")
-    .attr("width", width)
     .attr("height", height)
+    .attr("width", width)
+    .style("background-color", "lavender");
 
-  // bars
-  svg.selectAll("rect")
+  // BARS
+  svg.selectAll("rect.bar")
     .data(state.data)
     .join("rect")
+    .attr("class", "bar")
+    .attr("height", d => height - yScale(d.count))
     .attr("width", xScale.bandwidth())
-    .attr("height", d=> height - yScale(d.count))
-    .attr("x", d=>xScale(d.races))
-    .attr("y", d=> yScale(d.count))
+    .attr("x", d => xScale(d.races))
+    .attr("y", d => yScale(d.count))
 }
