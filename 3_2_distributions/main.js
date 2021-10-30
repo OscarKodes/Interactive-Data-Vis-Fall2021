9e5b4dde952ee5695b4f2791edb3fc95de354004
Oscar Ko <OscarKoEmail@gmail.com>
2021-10-30 14:34:42 -0400

    Create basic svg set up, add axis's

--- 3_2_distributions/main.js
+++ 3_2_distributions/main.js
@@ -1,24 +1,27 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.8,
+  height = window.innerHeight * 0.8,
+  margin = {
+    top: 50,
+    left: 50,
+    right: 50,
+    bottom: 50
+  },
+  radius = 5;
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
-// let svg;
-// let xScale;
-// let yScale;
-// let colorScale;
+let svg;
+let xScale;
+let yScale;
+let colorScale;
 
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selectedParty: "All" // + YOUR INITIAL FILTER SELECTION
+  selectedParty: "All" 
 };
 
 /* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
+d3.json("environmentRatings.json", d3.autoType).then(raw_data => {
   // + SET YOUR DATA PATH
   console.log("data", raw_data);
   // save our data to application state
@@ -30,19 +33,37 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 // this will be run *one time* when the data finishes loading in
 function init() {
   // + SCALES
+  xScale = d3.scaleLinear()
+    .domain([0, d3.max(state.data.map(d => d.ideologyScore2020))])
+    .range([margin.left, width - margin.right]);
 
+  yScale = d3.scaleLinear()
+    .domain([0, d3.max(state.data.map(d => d.envScore2020))])
+    .range([height - margin.top, margin.bottom]);
 
   // + AXES
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
 
 
   // + UI ELEMENT SETUP
 
 
   // + CREATE SVG ELEMENT
-
+  svg = d3.select("#container")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .style("background-color", "lavender");
 
   // + CALL AXES
+  svg.append("g")
+    .attr("transform", `translate(${0}, ${height - margin.bottom})`)
+    .call(xAxis);
 
+  svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${0})`)
+    .call(yAxis);
 
 
   draw(); // calls the draw function
@@ -53,20 +74,20 @@ function init() {
 function draw() {
 
   // + FILTER DATA BASED ON STATE
-  const filteredData = state.data
-    // .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
+  // const filteredData = state.data
+  //   // .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
 
-  const dot = svg
-    .selectAll("circle")
-    .data(filteredData, d => d.BioID)
-    .join(
-      // + HANDLE ENTER SELECTION
-      enter => enter,
+  // const dot = svg
+  //   .selectAll("circle")
+  //   .data(filteredData, d => d.BioID)
+  //   .join(
+  //     // + HANDLE ENTER SELECTION
+  //     enter => enter,
 
-      // + HANDLE UPDATE SELECTION
-      update => update,
+  //     // + HANDLE UPDATE SELECTION
+  //     update => update,
 
-      // + HANDLE EXIT SELECTION
-      exit => exit
-    );
+  //     // + HANDLE EXIT SELECTION
+  //     exit => exit
+  //   );
 }
